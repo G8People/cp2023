94ec671ae793b9c397fff689c2f5993e84bd279a
G8People <144767880+G8People@users.noreply.github.com>
2023-12-30 10:15:11 +0000

    新增w6內容

--- downloads/poland.c
+++ downloads/poland.c
@@ -1,5 +1,33 @@
-FILE *outputFile = fopen("./../images/japan_flag.png", "wb");
-if (outputFile == NULL) {
-    fprintf(stderr, "Error opening the output file.\n");
-    return 1;
+#include <stdio.h>
+#include <gd.h>
+
+int main() {
+    int width = 600;   // 設置寬度
+    int height = 400;  // 設置高度
+
+    gdImagePtr image = gdImageCreateTrueColor(width, height);
+
+    // 設置顏色
+    int white = gdImageColorAllocate(image, 255, 255, 255);
+    int red = gdImageColorAllocate(image, 255, 0, 0);
+
+    // 繪製白色背景
+    gdImageFilledRectangle(image, 0, 0, width - 1, height - 1, white);
+
+    // 計算紅條的高度（波蘭國旗有兩個紅條）
+    int stripeHeight = height / 2;
+
+    // 繪製兩個紅條
+    gdImageFilledRectangle(image, 0, 0, width - 1, stripeHeight - 1, red);
+    gdImageFilledRectangle(image, 0, height / 2, width - 1, height - 1, red);
+
+    // 輸出圖像
+    FILE *outputFile = fopen("poland_flag.png", "wb");
+    gdImagePng(image, outputFile);
+    fclose(outputFile);
+
+    // 釋放內存
+    gdImageDestroy(image);
+
+    return 0;
 }
