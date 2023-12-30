86e3da51952237e015a8672a2384c8e884504f0e
G8People <144767880+G8People@users.noreply.github.com>
2023-12-30 10:43:06 +0000

    修改內容

--- downloads/poland.c
+++ downloads/poland.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include <gd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+void drawPolandFlag(int width, int height) {
+    // ...（與之前的程式碼相同）
+}
+
+void createDestinationDirectory(const char *path) {
+    struct stat st = {0};
+
+    if (stat(path, &st) == -1) {
+        // 如果目錄不存在，則創建目錄
+        int status = mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+        if (status == 0) {
+            printf("目錄已成功建立。\n");
+        } else {
+            fprintf(stderr, "建立目錄時發生錯誤：%s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        printf("目錄已存在。\n");
+    }
+}
 
 int main() {
-    int width = 600;   // 設置寬度
-    int height = 400;  // 設置高度
+    int width = 16;   // 設置寬度
+    int height = 10;  // 設置高度
 
-    gdImagePtr image = gdImageCreateTrueColor(width, height);
+    // 建立目標目錄（請替換成實際的目錄路徑）
+    createDestinationDirectory("/path/to/destination/");
 
-    // 設置顏色
-    int white = gdImageColorAllocate(image, 255, 255, 255);
-    int red = gdImageColorAllocate(image, 255, 0, 0);
-
-    // 繪製白色背景
-    gdImageFilledRectangle(image, 0, 0, width - 1, height - 1, white);
-
-    // 計算紅條的高度（波蘭國旗有兩個紅條）
-    int stripeHeight = height / 2;
-
-    // 繪製兩個紅條
-    gdImageFilledRectangle(image, 0, 0, width - 1, stripeHeight - 1, red);
-    gdImageFilledRectangle(image, 0, height / 2, width - 1, height - 1, red);
-
-    // 輸出圖像
-    FILE *outputFile = fopen("poland_flag.png", "wb");
-    gdImagePng(image, outputFile);
-    fclose(outputFile);
-
-    // 釋放內存
-    gdImageDestroy(image);
+    // 繪製國旗並搬移到目標目錄
+    drawPolandFlag(width, height);
+    int result = system("mv poland_flag.png /path/to/destination/");
+    if (result != 0) {
+        fprintf(stderr, "搬移文件時發生錯誤。\n");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
