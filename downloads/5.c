36d3158a83bf915457e852427e58c4eefb92609c
G8People <144767880+G8People@users.noreply.github.com>
2023-12-30 17:51:13 +0000

    新增Ansic內容

---
+++ downloads/5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 林安翊\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : July 2, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0986-991578\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
